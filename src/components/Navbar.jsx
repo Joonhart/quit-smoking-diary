cf3b71dd5f7f08f4ba0fa8510e30d556e702f925
joonhart <joonhart@gmail.com>
2023-04-17 19:04:46 +0900

    css navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoLogoNoSmoking } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <header>
-      <Link to="/">
-        LOGO
-      </Link>
-      <nav>
-        <Link to="/">HOME</Link>
-        <Link to="goal">목표 설정</Link>
-        <Link to="stat">흡연 일지</Link>
-      </nav>
+    <header className="flex justify-between text-2xl border-b border-gray-300 p-5 bg-black text-white">
+      <div className="flex items-center">
+        <Link to="/">
+          <IoLogoNoSmoking />
+        </Link>
+        <nav className="flex items-center gap-6 pl-6 font-semibold">
+          <Link to="/">HOME</Link>
+          <Link to="goal">목표 설정</Link>
+          <Link to="stat">흡연 일지</Link>
+        </nav>
+      </div>
+      <div>
+        Login
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
