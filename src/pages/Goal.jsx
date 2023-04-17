d1db8cc7a62a76272cbc5c87e932f4469a0e7002
joonhart <joonhart@gmail.com>
2023-04-17 21:01:15 +0900

    text login logout

--- src/pages/Goal.jsx
+++ src/pages/Goal.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuthContext } from '../context/AuthContext'
 
 const Goal = () => {
+    const {user} = useAuthContext();
   return (
     <div>
-      Goal
+    {!user && <p>목표를 거창하게 세우는 곳입니다</p>}
+      {user && <p>너의 목표는...</p>}
     </div>
   )
 }
